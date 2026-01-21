d4e1cf19e19684ab04c57dc262a2f8a48ab29e98
James Jennings <jjennings308@gmail.com>
2026-01-21 12:03:13 -0500

    Added Pre-Login home page

--- core/views.py
+++ core/views.py
@@ -1,9 +1,22 @@
 # core/view.py
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from accounts.models import User, Profile
+from trips.models import Trip
 
 def home(request):
+    # Redirect authenticated users to dashboard
+    if request.user.is_authenticated:
+        return redirect('dashboard')  # or your post-login page
+    
+    context = {
+    'total_users': User.objects.filter(is_active=True).count(),
+    'total_trips': Trip.objects.count(),
+    'countries_count': Profile.objects.values('home_country').distinct().count(),
+    }
+    return render(request, 'home.html', context)
+
+def core_home(request):
     return render(request, "core/home.html")
 
 
